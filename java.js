bdd4fb2d73dcb5f5b062ac888afb19bd90801ae2
Sn1per010102 <117519518+Sn1per010102@users.noreply.github.com>
2024-04-20 13:46:09 +0300

    Update java.js

--- java.js
+++ java.js
@@ -1,1 +1,30 @@
-Hai
+let img = document.querySelector('.imgheader')
+let button = document.querySelector('.smena')
+function smen ()
+{
+ if (img.style.background == 'url(image.jpg)')
+ {
+   img.style.background = 'url(image2.jpg)'
+ }
+ else 
+ {
+    img.style.background = 'url(image.jpg)'
+ }
+
+}
+button.addEventListener('click', smen)
+
+ /*function change_theme() {
+    let header = document.querySelector('imgheader')
+    if (current_background == 'url(image.jpg)') {
+        current_background = 'url(image2.jpg)'
+    } else {
+        current_background = 'url(image.jpg)'
+    }
+    header.style.background = current_background
+    header.style.backgroundSize = 'cover'
+ }
+  
+ let change_theme_button = document.querySelector('.proIsmena')
+ let current_background = 'url(image.jpg)'
+ change_theme_button.addEventListener('click', change_theme)*/
