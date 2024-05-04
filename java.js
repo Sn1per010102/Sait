52b112c1a153dfa3ff8920db9a07aae3a24dc8b2
Sn1per010102 <117519518+Sn1per010102@users.noreply.github.com>
2024-05-04 18:08:59 +0300

    Update java.js

--- java.js
+++ java.js
@@ -1,30 +1,9 @@
-let img = document.querySelector('.imgheader')
-let button = document.querySelector('.smena')
-function smen ()
-{
- if (img.style.background == 'url(image.jpg)')
- {
-   img.style.background = 'url(image2.jpg)'
- }
- else 
- {
-    img.style.background = 'url(image.jpg)'
- }
-
-}
-button.addEventListener('click', smen)
-
- /*function change_theme() {
-    let header = document.querySelector('imgheader')
-    if (current_background == 'url(image.jpg)') {
-        current_background = 'url(image2.jpg)'
-    } else {
-        current_background = 'url(image.jpg)'
-    }
-    header.style.background = current_background
-    header.style.backgroundSize = 'cover'
- }
-  
- let change_theme_button = document.querySelector('.proIsmena')
- let current_background = 'url(image.jpg)'
- change_theme_button.addEventListener('click', change_theme)*/
+let button = document.getElementById('smena');
+let header = document.getElementById('header');
+let images = ['image2.jpg', 'Image.jpg'];
+let currentImageIndex = 0;
+function changeBackground() {
+      currentImageIndex = (currentImageIndex + 1) % images.length;
+      header.style.backgroundImage = `url('${images[currentImageIndex]}')`;
+  }
+button.addEventListener('click', changeBackground);
